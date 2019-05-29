a0d248f66af36ede97e54fc315b9530b58b75e36
Unknown <tramsyck@gmail.com>
2019-05-29 10:01:47 +0800

    1order+2peers完成

--- blockchain/query.go
+++ blockchain/query.go
@@ -3,16 +3,25 @@ package blockchain
 import (
 	"fmt"
 
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
+
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 )
 
+// QueryHello query the chaincode to get the state of hello
 func (setup *FabricSetup) QueryHello() (string, error) {
 	var args []string
 	args = append(args, "invoke")
 	args = append(args, "query")
 	args = append(args, "hello")
 
-	response, err := setup.client.Query(channel.Request{ChaincodeID: setup.ChainCodeID, Fcn: args[0], Args: [][]byte{[]byte(args[1]), []byte(args[2])}})
+	response, err := setup.client.Query(
+		channel.Request{
+			ChaincodeID: setup.ChainCodeID,
+			Fcn:         args[0],
+			Args:        [][]byte{[]byte(args[1]), []byte(args[2])}},
+		channel.WithRetry(retry.DefaultChannelOpts),
+		channel.WithTargetEndpoints("peer0.org1.example.com"))
 	if err != nil {
 		return "", fmt.Errorf("failed to query: %v", err)
 	}
